3b84f346dd2c2f4a1a8afa04d5760cb0676934be
mohitd504 <116343454+mohitd504@users.noreply.github.com>
2025-10-16 00:03:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-  backend "azurerm" {}
-}
-
+  
 provider "azurerm" {
   features {}
 }
